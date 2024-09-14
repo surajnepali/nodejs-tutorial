f59092aa92ea0893901e5d0b89f9dde8d27ddee3
Suraj Nepali <90302489+surajnepali@users.noreply.github.com>
2024-09-14 19:15:58 +0545

    edit: added /message in the app.js file

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY TEXT");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from Node.js</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 /**
  * .listen() method starts a process where nodejs will not immediately exit our script
